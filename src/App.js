c90ed83e153d905a3d154bee9e327fc302c577eb
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-01-18 17:49:12 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components /ExpenseItem";
+
+function App() {
+  return (
+    <div>
+        <h2>Let's get started!</h2>
+        <p>Let's get!</p>
+        <b>My Name is aiden</b>
+        <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
