6c94c6a5bd1b17b5779a651fbeda3ec326d256b8
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-01-18 18:06:03 -0700

    expense item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import ExpenseItem from "./components /ExpenseItem";
-import './components/ExpenseItem.css';
+import './ExpenseItem.css';
+
 function App() {
   return (
     <div>
-        <h2>Let's get started!</h2>
-        <p>Let's get!</p>
-        <b>My Name is aiden</b>
         <ExpenseItem/>
     </div>
   );
